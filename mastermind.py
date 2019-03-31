779a4de938bf3c31ff7967d5ca9a6baff450e04e
Alexandros Dorodoulis <aldor94@gmail.com>
2019-04-01 00:08:01 +0200

    chore: Merge all versions together

--- mastermind.py
+++ mastermind.py
@@ -1,9 +1,13 @@
-import itertools
-from random import choices, randint
 import json
+from functools import reduce
+from itertools import permutations, product, repeat
+from operator import iconcat
+from random import choices, randint
+from timeit import default_timer as timer
+
+import click
 import numpy as np
 
-from timeit import default_timer as timer
 space_size = 1296
 colors = [1, 2, 3, 4, 5, 6]
 # make the game
@@ -11,7 +15,7 @@ class Mastermind:
     # initialize the variables
     def __init__(self, goal):
         self.colors = colors
-        self.set = itertools.permutations(self.colors, 4)
+        self.set = permutations(self.colors, 4)
         self.goal = goal
         self.turn_counter = 0
         self.win = False
@@ -37,9 +41,9 @@ class Mastermind:
     def guess(self, combination, goal):
         white, black = self.get_white_and_black(combination, goal)
         answer = []
-        answer += itertools.repeat("B", black)
-        answer += itertools.repeat("W", white)
-        answer += itertools.repeat("nope", 4 - black - white)
+        answer += repeat("B", black)
+        answer += repeat("W", white)
+        answer += repeat("", 4 - black - white)
         self.turn_counter += 1
         # check for a win after each guess
         if answer == ["B", "B", "B", "B"]:
@@ -47,20 +51,17 @@ class Mastermind:
         return answer
 
     # GAME SOLVING FUNCTIONS STARTING HERE
-
-    # perform minimax algorithm to dataset
     def do_minimax(self, array):
-        var_list_2 = [10000] * self.space_size
-        for j in range(self.space_size):  # index j corresponds to unused data set
-            if array[5, j] != 1:
-                continue
 
-            var_list_1 = [0] * 14  # there are 14 possible answers to a code
-            for i in range(
-                self.space_size
-            ):  # index i corresponds to possible solution data set
-                if array[4, i] != 1:
-                    continue
+        solution_maxes = np.full([self.space_size], self.space_size + 1, dtype=int)
+
+        for j in np.where(array[5] == 1)[0]:
+            possible_solutions = np.zeros(
+                [14]
+            )  # there are 14 possible answers to a code
+            for i in np.where(array[4] == 1)[
+                0
+            ]:  # index i corresponds to possible solution data set
 
                 combination = array[0:4, i]
                 temp_goal = array[0:4, j]
@@ -68,37 +69,36 @@ class Mastermind:
 
                 # calculation of remaining combinations
                 if (white == 0) and (black == 4):
-                    var_list_1[0] += 1
+                    possible_solutions[0] += 1
                 elif (white == 0) and (black == 3):
-                    var_list_1[1] += 1
+                    possible_solutions[1] += 1
                 elif (white == 2) and (black == 2):
-                    var_list_1[2] += 1
+                    possible_solutions[2] += 1
                 elif (white == 1) and (black == 2):
-                    var_list_1[3] += 1
+                    possible_solutions[3] += 1
                 elif (white == 0) and (black == 2):
-                    var_list_1[4] += 1
+                    possible_solutions[4] += 1
                 elif (white == 3) and (black == 1):
-                    var_list_1[5] += 1
+                    possible_solutions[5] += 1
                 elif (white == 2) and (black == 1):
-                    var_list_1[6] += 1
+                    possible_solutions[6] += 1
                 elif (white == 1) and (black == 1):
-                    var_list_1[7] += 1
+                    possible_solutions[7] += 1
                 elif (white == 0) and (black == 1):
-                    var_list_1[8] += 1
+                    possible_solutions[8] += 1
                 elif (white == 4) and (black == 0):
-                    var_list_1[9] += 1
+                    possible_solutions[9] += 1
                 elif (white == 3) and (black == 0):
-                    var_list_1[10] += 1
+                    possible_solutions[10] += 1
                 elif (white == 2) and (black == 0):
-                    var_list_1[11] += 1
+                    possible_solutions[11] += 1
                 elif (white == 1) and (black == 0):
-                    var_list_1[12] += 1
+                    possible_solutions[12] += 1
                 elif (white == 0) and (black == 0):
-                    var_list_1[13] += 1
+                    possible_solutions[13] += 1
 
-            var_list_2[j] = max(var_list_1)
-        # print("Worst case scenario:", min(var_list_2), "options after this round.")
-        return array[0:4, np.argmin(var_list_2)]
+            solution_maxes[j] = np.amax(possible_solutions)
+        return array[0:4, np.argmin(solution_maxes)]
 
     # update possible solution dataset array based on code and code response (ground_truth)
     def update_solutions_dataset(self, array, tested_code, ground_truth):
@@ -111,7 +111,6 @@ class Mastermind:
             combination = array[0:4, i]
             white, black = self.get_white_and_black(combination, tested_code)
             # set cell to 0 if solution is incompatible with last feedback (black/white combination)
-
             if (
                 (ground_truth_white != white) or (ground_truth_black != black)
             ) or np.array_equal(combination, tested_code):
@@ -128,11 +127,9 @@ class Mastermind:
         array[5, unused_index] = 0  # 0 means code is not part of unused set anymore
 
 
-def run(goal):
+def run(goal, verbose):
     # initialize array with all possible combinations and information if part of solution set and information if part of unused set
-    # number_set = np.ones(dtype=int, shape=[6, space_size])
-    # thing=list()
-    thing = np.transpose(list(itertools.product(colors, repeat=4)))
+    thing = np.transpose(list(product(colors, repeat=4)))
 
     number_set = np.concatenate(
         (thing, np.ones(dtype=int, shape=[2, space_size])), axis=0
@@ -142,23 +139,23 @@ def run(goal):
 
     # initial guess
     combo = np.array([1, 1, 2, 2])
+    if verbose:
+        line = "|".join(str(x).center(12) for x in ["Turn", "Guess", "Outcome"])
+        click.echo(f"\n{line}")
+        click.echo("-" * (len(line) + 5))
 
     # start the turn clock, check for a win after each round
     while game.turn_counter < 12 and not game.win:
         # requires a string of ex. "1 1 1 1" to parse correctly.
-        # print("Give me a combination of the elements 1,2,3,4,5,6. Ya fucking idiot.")
-        # print(combo)
         # check if the guess is the valid format
         # don't uptick the turn counter for an invalid format
-        # i'm not that big of an asshole
         if len(combo) != 4 or not set(combo).issubset(game.colors):
-            raise (
-                "Invalid guess, moron. Make sure you're guessing four items from the list (e.g. '5 5 4 3')"
-            )
+            raise ("Something went wrong, I made an invalid guess")
         guess = game.guess(combo, game.goal)
-        # print(guess)
-        # print("goal is " + str(game.goal))
-
+        if verbose:
+            click.echo(
+                "|".join(str(x).center(12) for x in [game.turn_counter, combo, guess])
+            )
         game.update_solutions_dataset(number_set, combo, guess)
         # calculate number of solutions in the solution data set:
         possible_solutions = np.count_nonzero(number_set[4])
@@ -168,34 +165,113 @@ def run(goal):
             combo = number_set[0:4, np.argwhere(number_set[4] == 1)[0][0]]
         else:
             combo = game.do_minimax(number_set)
-    # if we leave that game loop, print the win/loss statement
-    # if game.win:
-    #     print("I guess that was a pretty good game. I guess.")
-    # else:
-    #     print(game.turn_counter)
-    #     print("Haha, loser.")
+    if verbose:
+        click.echo("\n")
     return {"result": game.win, "turns": game.turn_counter, "goal": goal}
 
 
-def main():
-    with open("test.txt", "a") as file:
-        file.write("[")
-        for goal in list(itertools.product(colors, repeat=4)):
-            time = []
-            for i in range(4):
+def singular_or_plural(param, text):
+    return f"{param} {text if param == 1 else text+'s'}"
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option(
+    "--file",
+    default="mastermind_test.json",
+    help="A file name to write the output of the test",
+    type=click.File(mode="a"),
+)
+@click.option("--time", default=False, help="Time each game", type=click.BOOL)
+@click.option(
+    "--verbose",
+    default=False,
+    help="Print verbose output of each round",
+    type=click.BOOL,
+)
+def test(file, time, verbose):
+    """This command plays a game with the AI for every possible combination.
+        If you want to also check the time it takes for a game to be played you can enable the --time=True flag
+    """
+    results = {"wins": 0, "loses": 0, "turns": [], "timings": []}
+    click.echo("  Starting the test")
+    file.write("[")
+    with click.progressbar(list(product(colors, repeat=4))) as bar:
+        for goal in bar:
+            timing = []
+            if time:
+                for i in range(4):
+                    start = timer()
+                    run(goal, verbose)
+                    end = timer()
+                    timing.append(end - start)
                 start = timer()
-                run(goal)
+                res = run(goal, verbose)
                 end = timer()
-                time.append(end-start)
-            start = timer()
-            res = run(goal)
-            end = timer()
-            time.append(end-start)
-            res["time"] = np.mean(time)
+                timing.append(end - start)
+                res["time"] = np.mean(timing)
+            else:
+                res = run(goal, verbose)
             file.write(json.dumps(res))
             file.write(",\n")
-        file.write("]")
+            results["wins" if res["result"] else "loses"] += 1
+            results["turns"].append(res["turns"])
+            if timing:
+                results["timings"].append(res["time"])
+    file.write("]")
+    click.echo(
+        f"""\n  Here are the results:
+  I won {singular_or_plural(results['wins'], 'time')}
+  I lost {singular_or_plural(results['loses'], 'time')}
+  The average turn took {np.mean(results['turns'])} moves"""
+    )
+    if time:
+        click.echo(
+        f"  The average time was {np.mean(results['timings'])} seconds \n"
+    )
+
+
+@cli.command()
+@click.option(
+    "--verbose",
+    default=False,
+    help="Print verbose output of each round",
+    type=click.BOOL,
+)
+def play(verbose):
+    """
+    This command plays a game with the AI.
+
+    You can enable verbose mode by using the --verbose=True flag
+    """
+    res = ""
+    while True:
+        try:
+            res = click.prompt("Please type 4 numbers from 1 till 6")
+            res = res.split(" ")
+            res = [r.split(",") for r in res if r]
+            res = reduce(iconcat, res, [])
+            res = [int(r) for r in res]
+        except ValueError:
+            click.echo(
+                "Invalid guess. Make sure you're guessing four items from the 1 till 6 (e.g. '5 5 4 3')"
+            )
+            continue
+        if len(res) != 4 or not set(res).issubset(colors):
+            click.echo(
+                "Invalid guess. Make sure you're guessing four items from the 1 till 6 (e.g. '5 5 4 3')"
+            )
+            continue
+        break
+    results = run(res, verbose)
+    click.echo(
+        f"I {'won' if results['result'] else 'lost'} this game after {singular_or_plural(results['turns'],'turn')}"
+    )
 
 
 if __name__ == "__main__":
-    main()
+    cli()
